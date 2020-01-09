4af5ad7c74181b524355558715c388e151503c03
Kalia1110 <56771744+Kalia1110@users.noreply.github.com>
2020-01-09 13:55:45 +0530

    inital commit

---
+++ Project15_DepandencyLookup-SingleIOC_Container/src/kcp/spring/IOCcontainer/IOC_Container.java
@@ -0,0 +1,20 @@
+package kcp.spring.IOCcontainer;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+@SuppressWarnings("deprecation")
+public class IOC_Container {
+    private static String path = "kcp/spring/configuration/applicationContext.xml";
+    static BeanFactory factory = null;
+    static {
+	factory = new XmlBeanFactory(new ClassPathResource(path));
+    }
+
+    public static BeanFactory getContainer() {
+
+	return factory;
+
+    }
+}
