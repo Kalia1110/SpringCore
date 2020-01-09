e212abee9bee18c214c604b289edf8decb1abe86
Kalia1110 <56771744+Kalia1110@users.noreply.github.com>
2020-01-09 13:56:13 +0530

    inital commit

---
+++ Project16_FactoryMethod/src/kcp/spring/test/FactoryMethodTest.java
@@ -0,0 +1,30 @@
+package kcp.spring.test;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+@SuppressWarnings("deprecation")
+public class FactoryMethodTest {
+
+    public static void main(String[] args) {
+	/*
+	 * DefaultListableBeanFactory factory=new DefaultListableBeanFactory();
+	 * XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader(factory);
+	 * reader.loadBeanDefinitions("kcp/spring/configuration/applicationContext.xml")
+	 * ;
+	 */
+	BeanFactory factory;
+	factory=new XmlBeanFactory(new ClassPathResource("kcp/spring/configuration/applicationContext.xml"));
+	Class c1=null;
+	c1=factory.getBean("c1",Class.class);
+	System.out.println(c1+" "+c1.getClass());
+	 Properties c2=factory.getBean("c2",Properties.class);
+	System.out.println(c2+" "+c2.getClass());
+    }
+
+}
